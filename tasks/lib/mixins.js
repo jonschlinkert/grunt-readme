d9b14d793aeb1616aaafc631ea2e39dfda410af0
Jon Schlinkert <dev@sellside.com>
2013-09-21 05:52:38 -0400

    add other assemble keywords to safename array

--- tasks/lib/mixins.js
+++ tasks/lib/mixins.js
@@ -21,12 +21,12 @@ exports = module.exports = {};
  * @example: "grunt-readme" => "readme"
  * @example: "helper-prettify" => "prettify"
  */
+
 exports.safename = function (name, patterns) {
   patterns = patterns || [];
   // return name.replace(/^(grunt|helper)[\-_]?/, '').replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
-  var prefixes = ["grunt"];
+  var prefixes = ["grunt", "helper", "mixin"];
   prefixes = grunt.util._.union([], prefixes, patterns);
   var re = new RegExp('^(?:' + prefixes.join('|') + ')[\-_]?');
   return name.replace(re, '').replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
 };
-
