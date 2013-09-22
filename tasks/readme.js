b73becf80bba6dec405522e3a8661ae5b1d44378
Jon Schlinkert <dev@sellside.com>
2013-09-22 15:06:38 -0400

    fixes #4

--- tasks/readme.js
+++ tasks/readme.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
       },
       sep: '\n',
       prefixes: [],
-      contributing: true
+      contributing: false
     });
 
     var resolve = options.resolve;
